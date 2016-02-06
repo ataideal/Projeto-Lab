9ba842dbd5ff55f5a2aa41cf1ef095b987f5150d
Ataíde <ataide.neto31@gmail.com>
2016-02-06 12:18:55 -0300

    Main Merge das structs e funções da feature-file pra Main.

--- Main.c
+++ Main.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+//--------------------------Estruturas-------------------------------//
+struct Data /* Essa estrutura define data */
+{
+	int dia,mes,ano;
+};
+
+typedef struct Data Data;
+
+struct Vendedor /* Essa estrutura define um vendedor*/
+{
+	int id,idade;
+	char nome[50],sexo[10],cpf[15];
+};
+typedef struct Vendedor Vendedor;
+
+struct Venda /* Essa estrutura define uma venda */
+{
+	int id,vendedorId;
+	Data data;
+	float valor;
+	char descricao[255];
+};
+typedef struct Venda Venda;
+//--------------------------Estruturas-------------------------------//
+
+//-------------------Protótipo das Funções---------------------------//
+FILE * lerArquivo (char path[]);
+int lerVendedores(Vendedor * vendedores,FILE * arquivo);
+//-------------------Protótipo das Funções---------------------------//
 
 int main (){
 	
