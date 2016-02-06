f77819139602011fcfffcbb1dcdfd74399984b86
Ataíde <ataide.neto31@gmail.com>
2016-02-06 12:15:15 -0300

    Feature-file, txts criados e gitignore Lendo vendedores pré-cadastrados

--- feature-file.c
+++ feature-file.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <string.h>
+//--------------------------Estruturas-------------------------------//
 struct Data /* Essa estrutura define data */
 {
 	int dia,mes,ano;
@@ -12,7 +13,7 @@ struct Vendedor /* Essa estrutura define um vendedor*/
 	int id,idade;
 	char nome[50],sexo[10],cpf[15];
 };
-typedef struct Venda Venda;
+typedef struct Vendedor Vendedor;
 
 struct Venda /* Essa estrutura define uma venda */
 {
@@ -22,15 +23,40 @@ struct Venda /* Essa estrutura define uma venda */
 	char descricao[255];
 };
 typedef struct Venda Venda;
+//--------------------------Estruturas-------------------------------//
+
+//-------------------Protótipo das Funções---------------------------//
+FILE * lerArquivo (char path[]);
+int lerVendedores(Vendedor * vendedores,FILE * arquivo);
+//-------------------Protótipo das Funções---------------------------//
 
 int main (){
+	FILE *arquivo;
+	arquivo = lerArquivo("Vendedores.txt");
 	
-	do{
-
-
-	}while(1);
-
-
-
+	Vendedor vendedores[10];
+	int quant_vendedores = lerVendedores(vendedores,arquivo);
+	fclose(arquivo);
+     
 	return 0;
 }
+
+//---------------------------Funções--------------------------------//
+FILE * lerArquivo (char path[]){ /* Esta função recebe como parametro a url do arquivo, faz a leitura do arquivo e retorna o ponteiro para função principal*/
+	FILE *fp;
+    	fp = fopen (path, "r");
+    	if (fp == NULL) {
+       		printf ("Houve um erro ao abrir o arquivo.\n");
+       		return NULL;
+    	}
+    	//printf ("Arquivo lido com sucesso\n");
+    	return fp;
+}
+
+int lerVendedores(Vendedor * vendedores,FILE * arquivo){ /* Esta função recebe o arquivo e o vetor de vendedores, e faz o parse de cada vendedor do arquivo para o vetor e retorna quantidade total de vendedores*/
+	int i=0;
+	while((fscanf(arquivo,"%d %d %s %s %s\n", &vendedores[i].id, &vendedores[i].idade, vendedores[i].nome,vendedores[i].sexo,vendedores[i].cpf))!=EOF )
+			i++;
+	return i;
+}
+//---------------------------Funções--------------------------------//
