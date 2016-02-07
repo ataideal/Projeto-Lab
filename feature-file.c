9bda961f8718a6ef7d2b306e6dc3ae4e8c0d76f3
Ataíde <ataide.neto31@gmail.com>
2016-02-07 19:59:38 -0300

    Feature-file atualizacao no menu Listando vendedores e atualizando na hora

--- feature-file.c
+++ feature-file.c
@@ -3,7 +3,7 @@
 //--------------------------Estruturas-------------------------------//
 struct Data /* Essa estrutura define data */
 {
-	char dia[3],mes[3],ano[5];
+	int dia,mes,ano;
 };
 
 typedef struct Data Data;
@@ -30,7 +30,7 @@ FILE * lerArquivo (char path[]);
 int lerVendedores(Vendedor * vendedores,FILE * arquivo);
 int lerVendas(Venda * vendas,FILE * arquivo);
 int incr_vendedor();
-void salvarVendedor(Vendedor vendedor);
+void salvarVendedor(Vendedor vendedor,Vendedor * vendedores,int * quant_vendedores);
 int incr_vendedor();
 void salvarVenda(Venda venda);
 //-------------------Protótipo das Funções---------------------------//
@@ -52,48 +52,75 @@ int main (){
 	quant_vendas = lerVendas(vendas,arquivo);
 	fclose(arquivo);
 	/*-------------------------------------------*/
+
+	int i;
+	for(i = 0;i<quant_vendedores;i++){
+		printf ("Vendedores id:%d\n",vendedores[i].id);
+		printf ("Nome: %s\nIdade: %d\nSexo: %s\nCpf:%s\n",vendedores[i].nome,vendedores[i].idade,vendedores[i].sexo,vendedores[i].cpf);
+	}
+
+	for(i = 0;i<quant_vendas;i++){
+		printf ("Vendas id:%d\n",vendas[i].id);
+		printf ("Vendedor id: %d\nData: %d/%d/%d\nValor: %f\nDescricao: %s\n",vendas[i].vendedorId,vendas[i].data.dia,vendas[i].data.mes,vendas[i].data.ano,vendas[i].valor,vendas[i].descricao);
+	}
+	
     /* ----- MENU ----*/
-
-    int opMenu;
-
-
+	int opMenu;
     do
     {
+    	system ("cls");
         printf ("Digite a opção desejada:\n");
-        printf ("1 - Cadastrar vendedor.\n");
-        printf ("2 - Cadastrar venda.\n");
+        printf ("1 - Cadastrar vendedor\n");
+        printf ("2 - Cadastrar venda\n");
         printf ("3 - Consultar as vendas de um funcionário em um determinado mês\n");
         printf ("4 - Consultar o total das vendas de um determinado vendedor\n");
         printf ("5 - Mostrar o número do vendedor que mais vendeu em um determinado mês\n");
         printf ("6 - Mostrar o número do mês com mais vendas\n");
-        printf ("0 - Finalizar o programa");
+        printf ("7 - Listar Vendedores\n");
+        printf ("8 - Listar Vendas\n");
+        printf ("0 - Finalizar o programa\n");
 
         printf ("Opção:  ");
         scanf ("%d", &opMenu);
-
+        getchar();
         system ("cls");
 
         int i;
         Vendedor vend; //Struct - Vendedor , vend variavel
-
+        Venda venda;
         switch (opMenu){
-
-
             case 0:
                 printf ("Saindo do menu.");
                 break;
             case 1:
-
-                printf ("Cadastre o vendedor:");
+                printf ("---Cadastre o vendedor---\n");
                 printf ("Digite o nome do vendedor: ");
-                scanf ("%s",vend.nome); //vend variavel, nome dentro da struct
+                scanf (" %[^\n]",vend.nome);
                 printf ("Digite a idade: ");
                 scanf ("%d",&vend.idade);
+                getchar();
+                printf ("Digite o sexo: ");
+                scanf ("%s",vend.sexo);
+                printf ("Digite o cpf: ");
+                scanf ("%s",vend.cpf);
                 vend.id = incr_vendedor();
-                salvarVendedor(vend);
+                salvarVendedor(vend,vendedores,&quant_vendedores);
+                getch();
                 break;
             case 2:
                 printf ("Cadastre a venda:");
+
+				/* --- Exemplo de como salvar uma venda --- */
+				venda.vendedorId = 1;
+				venda.data.dia = 1;
+				venda.data.mes = 2;
+				venda.data.mes = 2015;
+				venda.valor = 200.5;
+				strcpy(venda.descricao,"nois vende ou num vende clã?");
+				venda.id = incr_venda();
+				salvarVenda(venda);
+				getch();
+				/*------------------------------------------*/
                 break;
             case 3:
                 printf ("Consulta das vendas do funcionário desse mês.");
@@ -104,46 +131,33 @@ int main (){
             case 5:
                 break;
             case 6:
-
-                printf ("Lista de Vendas ");
-                for (i=0;i<quant_vendas;i++){
-                    printf ("Venda %d",i+1);
-                    printf ("Id da venda %d",vendas[i].id);
-                    printf ("Data da venda %s/%s/%s",vendas[i].data.dia,vendas[i].data.mes,vendas[i].data.ano);
-
+            	printf ("Numero do mês com mais vendas:");
+                break;
+			case 7:
+				printf ("Lista de Vendas - %d\n",quant_vendedores);
+				for(i = 0;i<quant_vendedores;i++){
+					printf ("Vendedores id:%d\n",vendedores[i].id);
+					printf ("Nome: %sIdade: %d\nSexo: %s\nCpf:%s\n",vendedores[i].nome,vendedores[i].idade,vendedores[i].sexo,vendedores[i].cpf);
+				}
+				getch();
+	            break;
+            case 8:
+                printf ("Lista de Vendas - %d",quant_vendas);
+                for(i = 0;i<quant_vendas;i++){
+					printf ("Vendas id:%d\n",vendas[i].id);
+					printf ("Vendedor id: %d\nData: %d/%d/%d\nValor: %f\nDescricao: %s\n",vendas[i].vendedorId,vendas[i].data.dia,vendas[i].data.mes,vendas[i].data.ano,vendas[i].valor,vendas[i].descricao);
                 }
-                printf ("Numero do mês com mais vendas:");
+                getch();
                 break;
             default:
                 printf ("Opção inválida!");
-            }
+        }
         printf ("\n");
     } while (opMenu!=0);
-
-
-
     /*-------------------------------------------*/
 
-    /* --- Exemplo de como salvar um vendedor --- */
-    /*
-	vend.idade = 15;
-	strcpy(vend.nome,"Ataide Neto");
-	strcpy(vend.sexo,"Masc");
-	strcpy(vend.cpf,"102030");
-    */
-	/*--------------------------------------------*/
 
-	/* --- Exemplo de como salvar uma venda --- */
-	Venda venda;
-	venda.id = incr_venda();
-	venda.vendedorId = 1;
-	strcpy(venda.data.dia,"01");
-	strcpy(venda.data.mes,"02");
-	strcpy(venda.data.mes,"2015");
-	venda.valor = 200.5;
-	strcpy(venda.descricao,"nois vende ou num vende clã?");
-	salvarVenda(venda);
-	/*------------------------------------------*/
+
 
 	return 0;
 }
@@ -172,12 +186,11 @@ int lerVendedores(Vendedor * vendedores,FILE * arquivo){ /* Esta função recebe
 
 int lerVendas(Venda * vendas,FILE * arquivo){ /* Esta função recebe o arquivo e o vetor de vendas, faz o parse de cada venda do arquivo para o vetor, e retorna quantidade total de vendas*/
 	int i=0;
-	while((fscanf(arquivo,"%d %d %s %s %s %f\n", &vendas[i].id, &vendas[i].vendedorId, vendas[i].data.dia,vendas[i].data.mes,vendas[i].data.ano,&vendas[i].valor))!=EOF ){
+	while((fscanf(arquivo,"%d %d %d %d %d %f\n", &vendas[i].id, &vendas[i].vendedorId, &vendas[i].data.dia,&vendas[i].data.mes,&vendas[i].data.ano,&vendas[i].valor))!=EOF ){
 		fgets (vendas[i].descricao,255,arquivo);
-		//printf ("%d %d %s %s %s %f %s", vendas[i].id, vendas[i].vendedorId, vendas[i].data.dia, vendas[i].data.mes, vendas[i].data.ano,vendas[i].valor,vendas[i].descricao);
+		printf ("%d %d %d %d %d %f %s", vendas[i].id, vendas[i].vendedorId, vendas[i].data.dia, vendas[i].data.mes, vendas[i].data.ano,vendas[i].valor,vendas[i].descricao);
 		i++;
 	}
-
 	return i;
 }
 
@@ -193,9 +206,20 @@ int incr_vendedor(){ /*Esta função incrementa o id do vendedor e o retorna */
 	return incr;
 }
 
-void salvarVendedor(Vendedor vendedor){ /*Esta função recebe um vendedor e o escreve no arquivo */
+void salvarVendedor(Vendedor vendedor,Vendedor * vendedores,int * quant_vendedores){ /*Esta função recebe um vendedor e o escreve no arquivo */
+	//Atualizando vetor de vendedores
+	int aux = (*quant_vendedores);
+	vendedores[aux].id = vendedor.id;
+	vendedores[aux].idade = vendedor.idade;
+	strcpy(vendedores[aux].sexo,vendedor.sexo);
+	strcpy(vendedores[aux].cpf,vendedor.cpf);
+	strcpy(vendedores[aux].nome,vendedor.nome);
+	strcpy(vendedores[aux].nome,"\n");
+	//Atualizado quantiade de vendedores
+	(*quant_vendedores) = (*quant_vendedores) + 1;
+	//Salvando no arquivo
 	FILE * fp = fopen ("Vendedores.txt","a");
-	int flag=fprintf (fp,"\n%d %d %s %s\n%s",vendedor.id, vendedor.idade,vendedor.sexo,vendedor.cpf,vendedor.nome);
+	int flag=fprintf (fp,"%d %d %s %s\n%s\n",vendedor.id, vendedor.idade,vendedor.sexo,vendedor.cpf,vendedor.nome);
 	fclose(fp);
 
 	if (flag<0)
@@ -219,13 +243,14 @@ int incr_venda(){ /*Esta função incrementa o id da venda e o retorna */
 
 void salvarVenda(Venda venda){ /*Esta função recebe um venda e o escreve no arquivo */
 	FILE * fp = fopen ("Vendas.txt","a");
-	int flag=fprintf (fp,"\n%d %d %s %s %s %f\n%s",venda.id, venda.vendedorId, venda.data.dia, venda.data.mes, venda.data.ano,venda.valor,venda.descricao);
+	int flag=fprintf (fp,"%d %d %d %d %d %f\n%s\n",venda.id, venda.vendedorId, venda.data.dia, venda.data.mes, venda.data.ano,venda.valor,venda.descricao);
 	fclose(fp);
 
 	if (flag<0)
 		printf ("Erro ao salvar!\n");
-	else
+	else{
 		printf ("Venda cadastrado com sucesso!\n");
-
+	}
 }
+
 //---------------------------Funções--------------------------------//
